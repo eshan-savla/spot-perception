2a9fbf5e2733e27b24fe10f67dde0ef9ae5c043e
Eshan <eshan.savla@gmail.com>
2024-05-07 11:25:42 +0200

    removed spot name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/robot/spot_ros2_ws/install/setup.bash
-ros2 launch spot_driver spot_driver.launch.py config_file:=./src/spot_ros2/configs/spot_ros_config.yaml spot_name:=hkaspot launch_rviz:=True publish_point_clouds:=True
+ros2 launch spot_driver spot_driver.launch.py config_file:=./src/spot_ros2/configs/spot_ros_config.yaml launch_rviz:=True publish_point_clouds:=True
+# spot_name:=hkaspot    
