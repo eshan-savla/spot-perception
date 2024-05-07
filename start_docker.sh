2a163d09ea53640fb3c20e6216fead7f44398836
Eshan <eshan.savla@gmail.com>
2024-05-07 10:32:51 +0200

    update start command for graphics

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run -it --rm --privileged eshansavla0512/ros2-spot-arm64:latest
+xhost + 
+docker pull eshansavla0512/ros2-spot-arm64:latest
+docker run -it --net=host \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --privileged eshansavla0512/ros2-spot-arm64:latest
