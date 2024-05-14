8f3f2f9c76df0d985068e8da52e855df2bcaec0b
Eshan <eshan.savla@gmail.com>
2024-05-14 16:06:17 +0200

    update configs for new namespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/robot/spot_ros2_ws/install/setup.bash
-ros2 launch spot_driver spot_driver.launch.py config_file:=./src/spot_ros2/configs/spot_ros_config.yaml launch_rviz:=True rviz_config_file:=/home/robot/spot-perception/configs/spot.rviz publish_point_clouds:=True spot_name:=hkaspot    
+ros2 launch spot_driver spot_driver.launch.py config_file:=./src/spot_ros2/configs/spot_ros_config.yaml publish_point_clouds:=True spot_name:=hkaspot    
