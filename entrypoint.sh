8c15494fe69ad608d855b645de350c92a9ac75e8
Eshan <eshan.savla@gmail.com>
2024-05-29 09:38:52 +0200

    docker rework installation and add dds_profile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 source /home/robot/spot_ros2_ws/install/setup.bash
 ros2 launch spot_driver spot_driver.launch.py config_file:=./src/spot_ros2/configs/spot_ros_config.yaml spot_name:=hkaspot &
 ros2 launch rtabmap_launch rtabmap.launch.py \
-    rtabmap_args:="--delete_db_on_start" \
     rgb_topic:=/hkaspot/camera/back/image \
     depth_topic:=/hkaspot/depth_registered/back/image \
     camera_info_topic:=/hkaspot/camera/back/camera_info \
