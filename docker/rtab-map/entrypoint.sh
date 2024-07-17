2e7578d990301ecc0c980f3a130312c47a15f291
Eshan Savla <eshan.savla@gmail.com>
2024-07-17 13:44:14 +0200

    fixed rtabmap

--- docker/rtab-map/entrypoint.sh
+++ docker/rtab-map/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /opt/ros/$ROS_DISTRO/setup.bash
+source /opt/ros/humble/setup.bash
 
 ros2 launch rtabmap_launch rtabmap.launch.py \
     rgb_topic:=/hkaspot/camera/back/image \
@@ -16,9 +16,9 @@ ros2 launch rtabmap_launch rtabmap.launch.py \
     approx_sync:=true \
     queue_size:=1000 \
     qos:=1 \
-    rviz:=true \
-    rtabmap_viz:=true \
-    localization:=true & 
+    rviz:=false \
+    rtabmap_viz:=false \
+    localization:=true &
 ros2 run rtabmap_util point_cloud_xyz --ros-args -r depth/image:=/hkaspot/depth_registered/frontleft/image -r depth/camera_info:=/hkaspot/depth_registered/frontleft/camera_info -p voxel_size:=0.05 -p decimation:=4 -p max_depth:=10.0 -p approx_sync:=false &
 ros2 run rtabmap_util obstacles_detection --ros-args -p frame_id:=hkaspot/body -p map_frame_id:=map &
 ros2 launch nav2_bringup navigation_launch.py params_file:=spot-perception/configs/nav2_params.yaml
